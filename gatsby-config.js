e1154f366ebc6960bb0ef644c0ab9032ec7c5334
Adriana Babakanian <ababakanian@stubhub.com>
2018-07-29 22:35:43 -0700

    functioning slugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,15 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
   ],
 }
