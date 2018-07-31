b54c0755a3ebcd800bc42b399b7be6e062228e51
Adriana Babakanian <ababakanian@stubhub.com>
2018-07-30 23:28:30 -0700

    configure markdown structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -53,36 +53,3 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     });
   });
 };
-
-// exports.createPages = ({ graphql, boundActionCreators }) => {
-//   const { createPage } = boundActionCreators
-//   return new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allMarkdownRemark {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-// ).then(result => {
-//       result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//         createPage({
-//           path: node.fields.slug,
-//           component: path.resolve(`./src/templates/blog-post.js`),
-//           context: {
-//             // Data passed to context is available in page queries as GraphQL variables.
-//             slug: node.fields.slug,
-//           },
-//         })
-//       })
-//       console.log(JSON.stringify(result, null, 4))
-//       resolve()
-//     })
-//   })
-// };
