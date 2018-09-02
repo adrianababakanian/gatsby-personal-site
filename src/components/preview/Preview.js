f9fde32ebe39fd6de2957580e9cf0fef9f4d3cb3
Adriana Babakanian <ababakanian@berkeley.edu>
2018-09-01 19:53:49 -0700

    fix for image link bug

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -5,7 +5,7 @@ import './Preview.scss'
 
 const Preview = ({ project, color, children }) => (
   <Link to={project.frontmatter.path} className={`preview ${color}`}>
-    { children }
+    <div className="preview-image"> { children } </div>
     <div className="preview-hover">
       { project.frontmatter.title }
     </div>
