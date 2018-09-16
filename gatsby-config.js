1dadf08504d552a87d1d40fe33da1cc235302b02
Adriana Babakanian <ababakanian@berkeley.edu>
2018-09-16 16:38:53 -0700

    remove typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,11 +48,5 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
   ],
 }
